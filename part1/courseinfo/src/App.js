01a0727d1010ec351e1a3aa39b1dc1cc69fb178f
lainem25 <mikael.2.laine@aalto.fi>
2022-12-15 01:30:21 +0200

    Completed 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -30,31 +30,32 @@ const Total = (props) => {
     return(
     <div>
         <p>
-                Number of exercises {props.exercises.reduce((a,b)=> a+b, 0)}  
+                Number of exercises {props.parts.map(item => item.exercises).reduce((a, b) => a + b)}  
         </p>
         </div>
     )
 }
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
+    const parts = [
+    {
         name: 'Fundamentals of React',
         exercises: 10
-    }
-    const part2 = {
+    },
+    {
         name: 'Using props to pass data',
         exercises: 7
-    }
-    const part3 = {
+    },
+    {
         name: 'State of a component',
         exercises: 14
     }
-
+    ]
     return (
         <div>
             <Header course={course} />
-            <Content parts={[part1, part2, part3]} />
-            <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     )
 }
