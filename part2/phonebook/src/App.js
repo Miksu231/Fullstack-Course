dc56a5765d329f4af3a5a58925b6335a56e3720e
lainem25 <mikael.2.laine@aalto.fi>
2023-01-04 03:30:04 +0200

    Completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import Name from './components/Name.js'
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
-  
 
+  const [search, changeSearch] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
@@ -15,17 +18,25 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearch = (event) => {
+    changeSearch(event.target.value)
+  }
   const addName = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName)) alert(`${newName} is already added to phonebook`)
     else {
-      setPersons(persons.concat({ name: newName , number: newNumber}))
+      setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length+1 }))
     }
   }
+  let list = (search === '') ? persons : persons.filter(person => {
+    return person.name.toLowerCase().includes(search.toLowerCase())
+  })
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with a <input value={search} onChange={handleSearch} /></div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -35,7 +46,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => <Name key={index} content={person.name} number={person.number} />)}
+        {list.map(person => <Name key={person.id} content={person.name} number={person.number}/>)}
       </ul>
     </div>
   )
